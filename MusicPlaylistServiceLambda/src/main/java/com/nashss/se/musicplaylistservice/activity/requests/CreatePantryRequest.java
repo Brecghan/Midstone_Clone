1563379c4de5328965c753ea71e7e9124a039fc3
Mark Bond <markbond615@gmail.com>
2023-09-01 15:03:06 -0500

    backend lambda classes created

---
+++ MusicPlaylistServiceLambda/src/main/java/com/nashss/se/musicplaylistservice/activity/requests/CreatePantryRequest.java
@@ -0,0 +1,60 @@
+package com.nashss.se.musicplaylistservice.activity.requests;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
+
+import java.util.List;
+
+import static com.nashss.se.musicplaylistservice.utils.CollectionUtils.copyToList;
+
+@JsonDeserialize(builder = CreatePantryRequest.Builder.class)
+public class CreatePantryRequest {
+    private final String pantryName;
+    private final String userId;
+
+    private CreatePantryRequest(String pantryName, String userId) {
+        this.pantryName = pantryName;
+        this.userId = userId;
+    }
+
+    public String getPantryName() {
+        return pantryName;
+    }
+
+
+    public String getUserId() {
+        return userId;
+    }
+
+    @Override
+    public String toString() {
+        return "CreatePantryRequest{" +
+                "pantryName='" + pantryName + '\'' +
+                ", userId='" + userId + '\'' +
+                '}';
+    }
+
+    //CHECKSTYLE:OFF:Builder
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    @JsonPOJOBuilder
+    public static class Builder {
+        private String pantryName;
+        private String userId;
+
+        public Builder withPantryName(String pantryName) {
+            this.pantryName = pantryName;
+            return this;
+        }
+
+        public Builder withUserId(String userId) {
+            this.userId = userId;
+            return this;
+        }
+        public CreatePantryRequest build() {
+            return new CreatePantryRequest(pantryName, userId);
+        }
+    }
+}
